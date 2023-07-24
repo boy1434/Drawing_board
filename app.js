70ec8bb04bf97a2f894dfb34a81fe3b390af2328
boy1434@naver.com <boy1434@naver.com>
2023-07-24 23:46:47 +0900

    그림 그리기 part.2

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
-
-const colors =[
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff"
-]
-
-function onclick(event) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+let isPaintng = false;
+function onMove(event) {
+    // 마우스를 누른 상태에서 커서를 움직이면 선을 그린다.
+    if(isPaintng){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    // 사용자가 canvas 위에 마우스가 올라가면 마우스의 위치로 좌표가 설정된다.
+    ctx.moveTo(event.offsetX, event.offsetY);
+    }
+// 마우스를 누르면 true
+function startPainting() {
+    isPaintng = true;
 }
-
-canvas.addEventListener("mousemove", onclick);
+// 마우스를 뗴면 false
+function cancelPainting() {
+    isPaintng = false;
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
