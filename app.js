dd059ca7b6a8389b59d1a4ec35b44498c9f13522
boy1434@naver.com <boy1434@naver.com>
2023-08-08 20:10:03 +0900

    색 추가 및 배경 색채우기

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const modeBtn = document.getElementById("mode-btn");
+const colorOption = Array.from(
+    document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -6,6 +10,7 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 let isPaintng = false;
+let isFilling = false;
 function onMove(event) {
     // 마우스를 누른 상태에서 커서를 움직이면 선을 그린다.
     if(isPaintng){
@@ -31,11 +36,33 @@ function onLineWidthChange(event){
 function onColorChacnge(event){
     ctx.strokeStyle = event.target.value;
 }
-
+function onColorClick(event){
+    ctx.strokeStyle = event.target.dataset.color;
+    ctx.fillStyle = event.target.dataset.color;
+}
+function onModeClick(){
+    if(isFilling){
+        isFilling = false
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true
+        modeBtn.innerText = "Draw";
+    }
+}
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChacnge);
+
+colorOption.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
